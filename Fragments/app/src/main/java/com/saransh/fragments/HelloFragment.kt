3f16a607d00112fa35a68a32af6d7f04b52224af
Saransh Jain <35698657+saransh219@users.noreply.github.com>
2020-03-30 11:36:28 +0530

    Fragments - using drag and drop

--- Fragments/app/src/main/java/com/saransh/fragments/HelloFragment.kt
+++ Fragments/app/src/main/java/com/saransh/fragments/HelloFragment.kt
@@ -1,7 +1,21 @@
 package com.saransh.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HelloFragment:Fragment() {
+class HelloFragment:Fragment()
+{
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View?
+    {
+
+        val view = inflater.inflate(R.layout.fragment_hello,container,false)
+
+        //here we can write all the custom codes
+        return  view
+
+    }
 
 }
