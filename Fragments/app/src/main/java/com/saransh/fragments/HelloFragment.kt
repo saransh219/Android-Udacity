c88ff1d030142f62380bb7f350ab5ba45b488049
Saransh Jain <35698657+saransh219@users.noreply.github.com>
2020-03-30 19:34:52 +0530

    Fragments - lifecycle of activity and fragments

--- Fragments/app/src/main/java/com/saransh/fragments/HelloFragment.kt
+++ Fragments/app/src/main/java/com/saransh/fragments/HelloFragment.kt
@@ -1,6 +1,8 @@
 package com.saransh.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +10,60 @@ import androidx.fragment.app.Fragment
 
 class HelloFragment:Fragment()
 {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("TAG","onAttach")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("TAG","onCreate")
+    }
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View?
     {
-
         val view = inflater.inflate(R.layout.fragment_hello,container,false)
-
         //here we can write all the custom codes
+        Log.i("TAG","onCreateView")
         return  view
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.i("TAG","onActivityCreated")
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.i("TAG","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("TAG","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("TAG","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("TAG","onStop")
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("TAG","onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("TAG","onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("TAG","onDetach")
     }
 
 }
--- Fragments/app/src/main/java/com/saransh/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/saransh/fragments/MainActivity.kt
@@ -2,19 +2,48 @@ package com.saransh.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("TAG","onCreate")
         setContentView(R.layout.activity_main)
 
-        val helloFragment = HelloFragment()
-        val fragmentManager = supportFragmentManager
-        //The beginTransaction() will return the object of the Fragment Transaction.
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.containerFL ,helloFragment,"helloFragment")
-        fragmentTransaction.commit()
+//        val helloFragment = HelloFragment()
+//        val fragmentManager = supportFragmentManager
+//        //The beginTransaction() will return the object of the Fragment Transaction.
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.add(R.id.containerFL ,helloFragment,"helloFragment")
+//        fragmentTransaction.commit()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("TAG","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("TAG","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("TAG","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("TAG","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("TAG","onDestroy")
+    }
+
 }
