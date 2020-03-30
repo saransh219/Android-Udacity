4d1afeff700d1b33eed6b6bd12ae62606ec60951
Saransh Jain <35698657+saransh219@users.noreply.github.com>
2020-03-30 12:39:36 +0530

    Fragments - using Fragment Manager

--- Fragments/app/src/main/java/com/saransh/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/saransh/fragments/MainActivity.kt
@@ -2,11 +2,19 @@ package com.saransh.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloFragment = HelloFragment()
+        val fragmentManager = supportFragmentManager
+        //The beginTransaction() will return the object of the Fragment Transaction.
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.containerFL ,helloFragment,"helloFragment")
+        fragmentTransaction.commit()
     }
 }
